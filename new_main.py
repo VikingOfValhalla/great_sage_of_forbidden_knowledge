7ddf2c8374c54dd8ccb8773b83ee445f104e473e
VikingOfValhalla <tom.wilson@nordtechsystems.com>
2022-06-20 20:01:56 -0400

    Updated new_main.py with a working websocket.

--- new_main.py
+++ new_main.py
@@ -1,15 +1,18 @@
 import websocket
 import json as json
 import os
+import time
+import rel
+import _thread
 from dotenv import load_dotenv
 
-class Websocket_Config:
 
+class WebSocket_Config:
     # Standard enviornment variables and general object creation
     load_dotenv()
     botToken = os.getenv("botToken")
-    ws = websocket.WebSocket()
-    
+    address = "wss://gateway.discord.gg/?v=6&encoding=json"
+
     # Payload information
     payload = {
         "op": 2,
@@ -24,55 +27,32 @@ class Websocket_Config:
         }
     }
 
-
-    def __init__(self, request, response):
-        self.request = request
-        self.response = response
-
-
-    def request_json(ws):
-        Websocket_Config.request = ws.send(json.dumps(Websocket_Config.payload))
-        
-
-
-    def receive_json(ws):
-        Websocket_Config.response = ws.recv()
-        if Websocket_Config.response:
-            return json.loads(Websocket_Config.response)
-
-
-def main():
     
-    request = Websocket_Config.request_json(Websocket_Config.ws)
-    print(request)
-    '''
-    response = Websocket_Config.response()
-    if response:
-        print(response)
-    '''
+def on_message(ws, message):
+    print("This is the start of the on_message_function")
+    print(message)
 
-'''
-def send_json_request(websocket, request):
-    websocket.send(json.dumps(request))
+def on_error(ws, error):
+    print(error)
 
-def receive_json_response(websocket):
-    response = websocket.recv()
-    if response:
-        return json.loads(response)
+def on_close(ws, close_status_code, close_msg):
+    print("### closed ###")
 
-
-def heartbeat(interval, websocket):
-    print("Heartbeat Begin")
-    while True:
-        time.sleep(interval)
-        heartbeat_json = {
-                "op": 1,
-                "d": "null"
-        }
-        send_json_request(websocket, heartbeat_json)
-        print("Heartbeat Sent")
-
-'''
+def on_open(ws):
+    ws.send(json.dumps(WebSocket_Config.payload))
+    print("Opened connection")
 
 if __name__ == "__main__":
-    main()
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp(WebSocket_Config.address,
+                              on_open=on_open,
+                              on_message=on_message,
+                              on_error=on_error,
+                              on_close=on_close)
+
+    ws.run_forever(dispatcher=rel)  # Set dispatcher to automatic reconnection
+    rel.signal(2, rel.abort)  # Keyboard Interrupt
+    rel.dispatch()
+
+
+
